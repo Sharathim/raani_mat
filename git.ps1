0833609bce23637e962b0aa8ff997fcab885568a
Sharathi M <rathisha273@gmail.com>
2026-09-06 00:35:00 +0530

    first commit

--- git.ps1
+++ git.ps1
@@ -2,5 +2,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/Shyam7379/raanimatrimony.git
+git remote add origin https://github.com/Sharathim/raani_mat.git
 git push -u origin main
