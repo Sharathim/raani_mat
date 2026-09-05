13db1f115d0cef1a45a690ad08bf84ec93b98915
Sharathi M <rathisha273@gmail.com>
2026-09-06 00:49:29 +0530

    feat: redirect issue fix

--- git.ps1
+++ git.ps1
@@ -1,6 +1,6 @@
 git init
 git add .
-git commit -m "feat: hosting success"
+git commit -m "feat: redirect issue fix"
 git branch -M main
 git remote add origin https://github.com/Sharathim/raani_mat.git
 git push -u origin main
