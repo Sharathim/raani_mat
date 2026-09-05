354e45f0ddb9bce3681099b55a8ff8b8d0dcd86d
Sharathi M <rathisha273@gmail.com>
2026-09-06 00:47:31 +0530

    feat: hosting success

--- git.ps1
+++ git.ps1
@@ -1,6 +1,6 @@
 git init
 git add .
-git commit -m "first commit"
+git commit -m "feat: hosting success"
 git branch -M main
 git remote add origin https://github.com/Sharathim/raani_mat.git
 git push -u origin main
