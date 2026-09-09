b34e8c1c2779ddb7d509212af1623efbbab60a64
Sharathi M <rathisha273@gmail.com>
2026-09-09 23:02:23 +0530

    feat: pwa12

--- git.ps1
+++ git.ps1
@@ -1,6 +1,6 @@
 git init
 git add .
-git commit -m "feat: pwa1"
+git commit -m "feat: pwa12"
 git branch -M main
 git remote add origin https://github.com/Sharathim/raani_mat.git
 git push -u origin main
