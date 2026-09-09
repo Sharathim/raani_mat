772944d2ee91869f83915edafebc3c68d32dcdba
Shyam Venkatesan <shyamhere2077@gmail.com>
2026-09-09 22:13:22 +0530

    feat: pwa1

--- git.ps1
+++ git.ps1
@@ -1,6 +1,6 @@
 git init
 git add .
-git commit -m "feat: pwa"
+git commit -m "feat: pwa1"
 git branch -M main
 git remote add origin https://github.com/Sharathim/raani_mat.git
 git push -u origin main
